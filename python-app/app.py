72003f50ac048df85e011f5c3fd4ba9aa18bf024
Robert Rodriguez <rrodriguez4570@gmail.com>
2025-04-11 02:54:49 -0700

    Testing pipeline

--- python-app/app.py
+++ python-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return f'Hello World from your local Minikube Cluster!'  # Proper string formatting
+    return f'Hello World from your local Minikube Cluster!This is a test'  # Proper string formatting
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
