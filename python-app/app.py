2036a9c4bcb929ed474b303bcea2eacb14d0d0c6
Robert Rodriguez <rrodriguez4570@gmail.com>
2025-04-11 02:47:16 -0700

    Edited app.py for pipeline test

--- python-app/app.py
+++ python-app/app.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # Get the POD_NAME environment variable, or use a default value if not set
-    pod_name = os.getenv("POD_NAME", "Unknown")
-    return f'Hello from AWS EKS pod {pod_name}, World!'  # Proper string formatting
+    return f'Hello World from your local Minikube Cluster!'  # Proper string formatting
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
